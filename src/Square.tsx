e12eae5b08c8b8448acf5a23e7689dde89722f8c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-04-18 22:10:11 +0300

    Describe the properties of a component using interface

--- src/Square.tsx
+++ src/Square.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { useState } from "react";
 import type { ClickHandler } from "./types";
 
+interface SquareProps {
+  value: string;
+  row: number;
+  column: number;
+  clickHandler: ClickHandler;
+}
+
 const Square = ({
   value,
   row,
   column,
   clickHandler
-}: {
-  value: string;
-  row: number;
-  column: number;
-  clickHandler: ClickHandler
-}) => {
+}: SquareProps) => {
   return (
     <button className="square"
       onClick={(_) => clickHandler(row, column)}
